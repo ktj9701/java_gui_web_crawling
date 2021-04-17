9345055ae243ef51b6b1c9fd1e09086e3b61c523
ktj97 <ktj97@DESKTOP-OLO68R9>
2021-04-17 14:15:57 +0900

    크롤링 과 웹페이지 불러오기

--- 일자리 다모아/src/Function/crolling.java
+++ 일자리 다모아/src/Function/crolling.java
@@ -1,7 +1,11 @@
 package Function;
 // 웹 크롤링 구현
 
+import java.awt.Desktop;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.Vector;
@@ -14,12 +18,14 @@ import org.jsoup.select.Elements;
 import GUI.Albatext;
 
 public class crolling {
-	public static String area="";
-	
+	public static String area = "";
+	public static ArrayList<String> URL = new ArrayList<String>();
+
 	public Vector<Albatext> crolling() {
+		int i = 0;
 		Vector<Albatext> arr = new Vector<>();
 
-		String url;	
+		String url;
 		int page = 1;
 		Document doc = null;
 		while (page != 4) { // 3페이지까지 출력
@@ -38,13 +44,17 @@ public class crolling {
 			Iterator<Element> ie4 = element.select("p.cTit").iterator();// 한마디
 			Iterator<Element> ie5 = element.select("td.recently").iterator();// 올라온 시간
 
-			while (ie1.hasNext()) {	
+			Iterator<Element> ie6 = element.select("p.cName").iterator();// 회사명
+			Iterator<Element> ie7 = element.select("p.cName").iterator();// 회사명
+			Iterator<Element> ie8 = element.select("p.cName").iterator();// 회사명
+			while (ie1.hasNext()) {
 				try {
-				arr.add(new Albatext(ie1.next().text().substring(3), ie2.next().text(), ie3.next().text(),
-						ie4.next().text(), ie5.next().text()));
-				}
-				catch(NoSuchElementException e) {
-					
+					arr.add(new Albatext(ie1.next().text().substring(3), ie2.next().text(), ie3.next().text(),
+							ie4.next().text(), ie5.next().text()));
+					URL.add(ie6.next().html().substring(9,
+							ie7.next().html().length() - ie8.next().text().length() - 6));
+
+				} catch (NoSuchElementException e) {
 				}
 			}
 			page++;
@@ -52,4 +62,13 @@ public class crolling {
 		return arr;
 	}
 
+	public static void explore(Object temp2) throws URISyntaxException {
+
+		try {
+			Desktop.getDesktop().browse(new URI((String) temp2));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
 }
