82e21555d0aaa2769eb0df3f0851d56cc28fe2bc
ktj97 <ktj97@DESKTOP-OLO68R9>
2021-04-20 00:37:24 +0900

    로그인 및 회원가입 관리

---
+++ 일자리 다모아/src/Function/DB_Method.java
@@ -0,0 +1,42 @@
+package Function;
+
+import java.sql.SQLException;
+
+public class DB_Method {
+	private DB db;
+
+	public DB_Method() {
+		db = DB.getInstance();
+	}
+
+	public boolean Login(String ID, String PWD) throws SQLException {  //로그인
+		String query = "SELECT * FROM Login";
+		DB.stmt = DB.conn.createStatement();
+		if (DB.stmt.execute(query)) {
+			DB.rs = DB.stmt.getResultSet();
+		}
+		while (DB.rs.next()) {
+			if (DB.rs.getString("ID").equals(ID) && DB.rs.getString("PWD").equals(PWD)) {
+				System.out.println("Login Success");
+				 DB.stmt.close();
+				 DB.rs.close();
+				return true;
+			}
+		}
+		System.out.println("Login Fail");
+		 DB.stmt.close();
+		DB.rs.close();
+		return false;
+	}
+
+	public boolean Join(String ID, String PWD){      //회원가입
+		String query = "INSERT INTO Login VALUES(" + "'" + ID + "'," + "'" + PWD + "'" + ")";
+		try {
+			DB.stmt.executeUpdate(query);
+			System.out.println("Join Success");
+		} catch (SQLException e) {
+			System.out.println("Join Fail");
+		}	
+		return false;
+	}
+}
