ee207696f4df564fd1a980e68d2e00d95216a945
ktj97 <ktj97@DESKTOP-OLO68R9>
2021-04-20 00:36:10 +0900

    알바몬 크롤링

--- 일자리 다모아/src/Function/crolling.java
+++ 일자리 다모아/src/Function/crolling.java
@@ -5,7 +5,6 @@ import java.awt.Desktop;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.Vector;
@@ -19,11 +18,9 @@ import GUI.Albatext;
 
 public class crolling {
 	public static String area = "";
-	public static ArrayList<String> URL = new ArrayList<String>();
 
 	public Vector<Albatext> crolling() {
-		int i = 0;
-		Vector<Albatext> arr = new Vector<>();
+		Vector<Albatext> albamon = new Vector<>();
 
 		String url;
 		int page = 1;
@@ -37,33 +34,29 @@ public class crolling {
 				e.printStackTrace();
 			}
 			Elements element = doc.select("div.gListWrap");
-
 			Iterator<Element> ie1 = element.select("td.area").iterator();// 지역
 			Iterator<Element> ie2 = element.select("p.won").iterator();// 급여
 			Iterator<Element> ie3 = element.select("p.cName").iterator();// 회사명
-			Iterator<Element> ie4 = element.select("p.cTit").iterator();// 한마디
+			Iterator<Element> ie4 = element.select("p.cTit").iterator();// 내용
 			Iterator<Element> ie5 = element.select("td.recently").iterator();// 올라온 시간
-
-			Iterator<Element> ie6 = element.select("p.cName").iterator();// 회사명
-			Iterator<Element> ie7 = element.select("p.cName").iterator();// 회사명
-			Iterator<Element> ie8 = element.select("p.cName").iterator();// 회사명
+			
+			Iterator<Element> ie6 = element.select("p.cName").iterator();// URL뽑아내기1
+			Iterator<Element> ie7 = element.select("p.cName").iterator();// URL뽑아내기2
+			Iterator<Element> ie8 = element.select("p.cName").iterator();// URL뽑아내기3
 			while (ie1.hasNext()) {
 				try {
-					arr.add(new Albatext(ie1.next().text().substring(3), ie2.next().text(), ie3.next().text(),
-							ie4.next().text(), ie5.next().text()));
-					URL.add(ie6.next().html().substring(9,
-							ie7.next().html().length() - ie8.next().text().length() - 6));
-
+					albamon.add(new Albatext(ie1.next().text().substring(3), ie2.next().text(), ie3.next().text(),
+							ie4.next().text(), ie5.next().text(),ie6.next().html().substring(9,ie7.next().html().length() - ie8.next().text().length() - 6)));
 				} catch (NoSuchElementException e) {
 				}
 			}
 			page++;
 		}
-		return arr;
+		return albamon;
 	}
 
 	public static void explore(Object temp2) throws URISyntaxException {
-
+System.out.println(temp2);
 		try {
 			Desktop.getDesktop().browse(new URI((String) temp2));
 		} catch (IOException e) {
