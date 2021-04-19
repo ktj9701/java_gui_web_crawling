d7933e37956f2223650c888555d093b034f1274c
ktj97 <ktj97@DESKTOP-OLO68R9>
2021-04-20 00:37:45 +0900

    알바천국 페이지

---
+++ 일자리 다모아/src/GUI/AlbaHeavenpage.java
@@ -0,0 +1,101 @@
+package GUI;
+
+import java.util.Vector;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import Function.wewe;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.net.URISyntaxException;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AlbaHeavenpage extends JFrame {
+	static wewe Albaheaven_crolling = new wewe();
+	static JTable table; // 테이블
+	static DefaultTableModel TableModel = new DefaultTableModel() {// 테이블 내용 수정 불가
+		public boolean isCellEditable(int rowIndex, int mCollndex) {
+			return false;
+		}
+	}; // 내용 수정 불가
+	public static DefaultComboBoxModel ComboModel = new DefaultComboBoxModel();
+	static String head[] = { "지역", "급여", "회사명", "내용", "등록 시간" };
+	public static JScrollPane scroll;
+	static Vector<Albatext> AlbaHeaven;
+	static Object arr[][];
+
+	Buttonlistener listener = new Buttonlistener();
+	MouseListener MouseListener = new MouseListener();
+
+	public AlbaHeavenpage() {
+		setTitle("일자리 다모아 - 팝업");
+		setSize(1500, 800);
+		this.setLayout(null);
+		setVisible(true);
+		setResizable(false); // 프로그램 크기 조절 불가 설정
+		setLocationRelativeTo(null); // 화면 중앙에 오도록 하는 설정
+		
+		table = new JTable(TableModel);
+		table.addMouseListener(MouseListener);
+		Search_AlbaHeaven();
+		scroll = new JScrollPane(table);
+		scroll.setBounds(160, 130, 1325, 633);
+		scroll.setVisible(true);
+		add(scroll);
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	class Buttonlistener implements ActionListener { // 버튼 이벤트
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
+	public class MouseListener extends MouseAdapter {
+
+		public void mouseClicked(MouseEvent e) {
+
+			if (e.getClickCount() == 2) {
+				try {
+					Function.crolling.explore(AlbaHeaven.get(table.getSelectedRow()).getURL());
+				} catch (URISyntaxException e1) {
+					e1.printStackTrace();
+				}
+			} // 더블클릭
+
+		}
+	}
+	public static void Search_AlbaHeaven() { // 광고 검색
+		AlbaHeaven = Albaheaven_crolling.crolling();
+		arr = new Object[AlbaHeaven.size()][5];
+
+		for (int i = 0; i < AlbaHeaven.size(); i++) {
+			int j = 0;
+			arr[i][j++] = AlbaHeaven.get(i).getArea();
+			arr[i][j++] = AlbaHeaven.get(i).getPay();
+			arr[i][j++] = AlbaHeaven.get(i).getOffice();
+			arr[i][j++] = AlbaHeaven.get(i).getText();
+			arr[i][j++] = AlbaHeaven.get(i).getTime();
+
+		}
+		TableModel.setDataVector(arr, head);
+		table.getColumnModel().getColumn(0).setPreferredWidth(170);
+		table.getColumnModel().getColumn(1).setPreferredWidth(170);
+		table.getColumnModel().getColumn(2).setPreferredWidth(360);
+		table.getColumnModel().getColumn(3).setPreferredWidth(525);
+		table.getColumnModel().getColumn(4).setPreferredWidth(100);
+		table.getTableHeader().setReorderingAllowed(false);
+		table.setRowHeight(35);
+		table.setFont(new Font("맑은 고딕", Font.BOLD, 25)); // 글자 크기 설정
+	}
+}
