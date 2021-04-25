fe31cf6d52560fa88f84aa8eed6604348c4f6531
ktj97 <ktj97@DESKTOP-OLO68R9>
2021-04-25 22:15:31 +0900

    데이터베이스 기능

--- 일자리 다모아/src/Function/DB_Method.java
+++ 일자리 다모아/src/Function/DB_Method.java
@@ -4,12 +4,10 @@ import java.sql.SQLException;
 
 public class DB_Method {
 	private DB db;
-
 	public DB_Method() {
 		db = DB.getInstance();
 	}
-
-	public boolean Login(String ID, String PWD) throws SQLException {  //로그인
+	public boolean Login(String ID, String PWD) throws SQLException { // 로그인
 		String query = "SELECT * FROM Login";
 		DB.stmt = DB.conn.createStatement();
 		if (DB.stmt.execute(query)) {
@@ -18,25 +16,32 @@ public class DB_Method {
 		while (DB.rs.next()) {
 			if (DB.rs.getString("ID").equals(ID) && DB.rs.getString("PWD").equals(PWD)) {
 				System.out.println("Login Success");
-				 DB.stmt.close();
-				 DB.rs.close();
+				DB.stmt.close();
+				DB.rs.close();
 				return true;
 			}
 		}
 		System.out.println("Login Fail");
-		 DB.stmt.close();
+		DB.stmt.close();
 		DB.rs.close();
 		return false;
 	}
 
-	public boolean Join(String ID, String PWD){      //회원가입
-		String query = "INSERT INTO Login VALUES(" + "'" + ID + "'," + "'" + PWD + "'" + ")";
-		try {
+	public boolean Join(String ID, String PWD) throws SQLException { // 회원가입
+		String query = "SELECT * FROM Login";
+		DB.stmt = DB.conn.createStatement();
+		if (DB.stmt.execute(query)) {
+			DB.rs = DB.stmt.getResultSet();
+		}
+		while (DB.rs.next()) {
+			if (DB.rs.getString("ID").equals(ID)) {
+				System.out.println("Join Fail");
+				return false;
+			}
+		}
+		 query = "INSERT INTO Login VALUES(" + "'" + ID + "'," + "'" + PWD + "'" + ")";
 			DB.stmt.executeUpdate(query);
 			System.out.println("Join Success");
-		} catch (SQLException e) {
-			System.out.println("Join Fail");
-		}	
-		return false;
+		return true;
 	}
 }
