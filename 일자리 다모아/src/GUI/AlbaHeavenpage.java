a5e9bb028004fac70739b068f85f3bab13a70020
ktj97 <ktj97@DESKTOP-OLO68R9>
2021-04-25 22:15:42 +0900

    알바천국페이지

--- 일자리 다모아/src/GUI/AlbaHeavenpage.java
+++ 일자리 다모아/src/GUI/AlbaHeavenpage.java
@@ -40,7 +40,7 @@ public class AlbaHeavenpage extends JFrame {
 		setVisible(true);
 		setResizable(false); // 프로그램 크기 조절 불가 설정
 		setLocationRelativeTo(null); // 화면 중앙에 오도록 하는 설정
-		
+
 		table = new JTable(TableModel);
 		table.addMouseListener(MouseListener);
 		Search_AlbaHeaven();
@@ -50,17 +50,11 @@ public class AlbaHeavenpage extends JFrame {
 		add(scroll);
 	}
 
-	
-	
-	
-	
-	
-	
-	
 	class Buttonlistener implements ActionListener { // 버튼 이벤트
 		public void actionPerformed(ActionEvent e) {
 		}
 	}
+
 	public class MouseListener extends MouseAdapter {
 
 		public void mouseClicked(MouseEvent e) {
@@ -75,27 +69,31 @@ public class AlbaHeavenpage extends JFrame {
 
 		}
 	}
+
 	public static void Search_AlbaHeaven() { // 광고 검색
-		AlbaHeaven = Albaheaven_crolling.crolling();
-		arr = new Object[AlbaHeaven.size()][5];
+		try {
+			AlbaHeaven = Albaheaven_crolling.crolling();
+			arr = new Object[AlbaHeaven.size()][5];
 
-		for (int i = 0; i < AlbaHeaven.size(); i++) {
-			int j = 0;
-			arr[i][j++] = AlbaHeaven.get(i).getArea();
-			arr[i][j++] = AlbaHeaven.get(i).getPay();
-			arr[i][j++] = AlbaHeaven.get(i).getOffice();
-			arr[i][j++] = AlbaHeaven.get(i).getText();
-			arr[i][j++] = AlbaHeaven.get(i).getTime();
+			for (int i = 0; i < AlbaHeaven.size(); i++) {
+				int j = 0;
+				arr[i][j++] = AlbaHeaven.get(i).getArea();
+				arr[i][j++] = AlbaHeaven.get(i).getPay();
+				arr[i][j++] = AlbaHeaven.get(i).getOffice();
+				arr[i][j++] = AlbaHeaven.get(i).getText();
+				arr[i][j++] = AlbaHeaven.get(i).getTime();
 
+			}
+			TableModel.setDataVector(arr, head);
+			table.getColumnModel().getColumn(0).setPreferredWidth(170);
+			table.getColumnModel().getColumn(1).setPreferredWidth(170);
+			table.getColumnModel().getColumn(2).setPreferredWidth(360);
+			table.getColumnModel().getColumn(3).setPreferredWidth(525);
+			table.getColumnModel().getColumn(4).setPreferredWidth(100);
+			table.getTableHeader().setReorderingAllowed(false);
+			table.setRowHeight(35);
+			table.setFont(new Font("맑은 고딕", Font.BOLD, 25)); // 글자 크기 설정
+		} catch (NullPointerException e) {
 		}
-		TableModel.setDataVector(arr, head);
-		table.getColumnModel().getColumn(0).setPreferredWidth(170);
-		table.getColumnModel().getColumn(1).setPreferredWidth(170);
-		table.getColumnModel().getColumn(2).setPreferredWidth(360);
-		table.getColumnModel().getColumn(3).setPreferredWidth(525);
-		table.getColumnModel().getColumn(4).setPreferredWidth(100);
-		table.getTableHeader().setReorderingAllowed(false);
-		table.setRowHeight(35);
-		table.setFont(new Font("맑은 고딕", Font.BOLD, 25)); // 글자 크기 설정
 	}
 }
