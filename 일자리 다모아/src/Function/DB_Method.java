04c97530031dabf3b88baec31bc0ac75a7a2c22e
ktj97 <ktj97@DESKTOP-OLO68R9>
2021-04-28 09:12:23 +0900

    db메소드

--- 일자리 다모아/src/Function/DB_Method.java
+++ 일자리 다모아/src/Function/DB_Method.java
@@ -1,47 +1,75 @@
 package Function;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
-public class DB_Method {
-	private DB db;
-	public DB_Method() {
-		db = DB.getInstance();
+public class DB_Method{
+		static Connection conn;
+		static Statement stmt = null;
+		static ResultSet rs = null;
+		
+		public DB_Method() {
+		
+			try {
+				Class.forName("org.gjt.mm.mysql.Driver");
+			
+			} catch (Exception e) {
+				System.out.println("fail");
+			}
 	}
+
 	public boolean Login(String ID, String PWD) throws SQLException { // 로그인
+		conn=null;
 		String query = "SELECT * FROM Login";
-		DB.stmt = DB.conn.createStatement();
-		if (DB.stmt.execute(query)) {
-			DB.rs = DB.stmt.getResultSet();
-		}
-		while (DB.rs.next()) {
-			if (DB.rs.getString("ID").equals(ID) && DB.rs.getString("PWD").equals(PWD)) {
-				System.out.println("Login Success");
-				DB.stmt.close();
-				DB.rs.close();
+		conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/Login?useSSL=false", "root", "1234");
+		stmt = conn.createStatement();
+		//if (DB.stmt.execute(query)) {
+		//	DB.rs = DB.stmt.getResultSet();
+		//}
+		rs=stmt.executeQuery(query);
+		while (rs.next()) {
+			if (rs.getString("ID").equals(ID) && rs.getString("PWD").equals(PWD)) {
+				System.out.println("Login Success");	
+				rs.close();
+				stmt.close();
+				conn.close();
 				return true;
 			}
 		}
 		System.out.println("Login Fail");
-		DB.stmt.close();
-		DB.rs.close();
+	
+		rs=null;
+		stmt=null;
+
 		return false;
 	}
 
 	public boolean Join(String ID, String PWD) throws SQLException { // 회원가입
+		conn=null;
+		conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/Login?useSSL=false", "root", "1234");
+		stmt = conn.createStatement();
 		String query = "SELECT * FROM Login";
-		DB.stmt = DB.conn.createStatement();
-		if (DB.stmt.execute(query)) {
-			DB.rs = DB.stmt.getResultSet();
-		}
-		while (DB.rs.next()) {
-			if (DB.rs.getString("ID").equals(ID)) {
+		stmt = conn.createStatement();
+		//if (DB.stmt.execute(query)) {
+		//	DB.rs = DB.stmt.getResultSet();
+		//}
+		rs=stmt.executeQuery(query);
+		while (rs.next()) {
+			if (rs.getString("ID").equals(ID)) {
 				System.out.println("Join Fail");
 				return false;
 			}
 		}
 		 query = "INSERT INTO Login VALUES(" + "'" + ID + "'," + "'" + PWD + "'" + ")";
-			DB.stmt.executeUpdate(query);
+		stmt.executeUpdate(query);
 			System.out.println("Join Success");
+			
+			stmt=null;
+			rs=null;
+
 		return true;
 	}
 }
