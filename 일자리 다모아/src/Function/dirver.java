3d412ef2c7b19505278de04f61d15d66f9e1f13b
ktj97 <ktj97@DESKTOP-OLO68R9>
2021-05-08 14:48:16 +0900

    시동

--- 일자리 다모아/src/Function/dirver.java
+++ 일자리 다모아/src/Function/dirver.java
@@ -1,4 +1,7 @@
 package Function;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import GUI.Loginpage;
 import GUI.Mainpage;
 
@@ -6,6 +9,7 @@ public class dirver {
 	public static final int SCREEN_WIDTH=1024;  //가로길이
 	public static final int SCREEN_HEIGHT=720; // 세로길이
 	public static void main(String[] args) {
+
 		new Loginpage();
 	} 
 }
