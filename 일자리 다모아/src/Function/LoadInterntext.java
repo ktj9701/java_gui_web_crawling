d4648d2a6ae851b8d54d018e7c6e93bbe9897449
ktj97 <ktj97@DESKTOP-OLO68R9>
2021-05-12 19:50:19 +0900

    저장한 인턴 백터 불러오기

---
+++ 일자리 다모아/src/Function/LoadInterntext.java
@@ -0,0 +1,59 @@
+package Function;
+
+public class LoadInterntext {
+	private String name;
+	private String project;
+	private String area;
+	private String state;
+	private String num;
+	private String url;
+
+	public LoadInterntext(String name, String project, String area, String num,String state) {
+		this.name = name;
+		this.project = project;
+		this.area = area;
+		this.num = num;
+		this.state = state;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getProject() {
+		return project;
+	}
+
+	public void setProject(String project) {
+		this.project = project;
+	}
+
+	
+	public String getArea() {
+		return area;
+	}
+
+	public void setArea(String area) {
+		this.area = area;
+	}
+
+	public String getNum() {
+		return num;
+	}
+
+	public void setNum(String num) {
+		this.num = num;
+	}
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+}
