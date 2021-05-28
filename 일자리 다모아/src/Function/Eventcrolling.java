97194d4acfad0bed652e9c21540d54131d939a3f
ktj97 <ktj97@DESKTOP-OLO68R9>
2021-05-28 20:15:21 +0900

    이벤트크롤링

--- 일자리 다모아/src/Function/Eventcrolling.java
+++ 일자리 다모아/src/Function/Eventcrolling.java
@@ -19,28 +19,61 @@ public class Eventcrolling {
 	public Vector<Eventtext> crolling() {
 		Vector<Eventtext> alba_event = new Vector<>();
 		String url;
-		
+
 		int i = 0;
 		Document doc = null;
 
-			url = "http://www.alba.co.kr/story/event/EventList.asp";
-			try {
-				doc = Jsoup.connect(url).get();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			Elements element = doc.select("div.eventIng");
-			Iterator<Element> ie1 = element.select("p.title").iterator();// 제목			
-			Iterator<Element> ie2 = element.select("p.content").iterator();// 내용
-			Iterator<Element> ie3 = element.select("dd.term").iterator();// 기간
-			Iterator<Element> ie4 = element.select("dd.announce").iterator();// 당첨자발표기간
-			Iterator<Element> ie5 = element.select("p.title").iterator();// url
+		url = "http://www.alba.co.kr/story/event/EventList.asp";
 
-			while (ie1.hasNext()) {
-				String temp[] = ie5.next().html().split("\"");
-				alba_event.add(new Eventtext(ie1.next().text(), ie2.next().text(), ie3.next().text(), ie4.next().text(),
-						temp[1]));
-			}
+		try { // 알바천국 설문조사
+			doc = Jsoup.connect(url).get();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Elements element = doc.select("div.surveyIng");
+		Iterator<Element> ie1 = element.select("p.title").iterator();// 제목
+		Iterator<Element> ie2 = element.select("p.content").iterator();// 내용
+		Iterator<Element> ie3 = element.select("dd.term").iterator();// 기간
+		Iterator<Element> ie5 = element.select("p.title").iterator();// url
+
+		while (ie1.hasNext()) {
+			String temp[] = ie5.next().html().split("\"");
+			alba_event.add(new Eventtext(ie1.next().text(), ie2.next().text(), ie3.next().text(), "설문조사", temp[1]));
+		}
+
+		try { // 알바천국 이벤트
+			doc = Jsoup.connect(url).get();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		element = doc.select("div.eventIng");
+		ie1 = element.select("p.title").iterator();// 제목
+		ie2 = element.select("p.content").iterator();// 내용
+		ie3 = element.select("dd.term").iterator();// 기간
+		ie5 = element.select("p.title").iterator();// url
+
+		while (ie1.hasNext()) {
+			String temp[] = ie5.next().html().split("\"");
+			alba_event.add(new Eventtext(ie1.next().text(), ie2.next().text(), ie3.next().text(), "이벤트", temp[1]));
+		}
+
+		url = "http://www.albamon.com/community/Event/Event_list";
+		try { // 알바몬 이벤트
+			doc = Jsoup.connect(url).get();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		element = doc.select("div.eventList");
+		ie1 = element.select("span.txt").iterator();// 제목
+		ie2 = element.select("span.txt").iterator();// 내용
+		ie3 = element.select("span.date").iterator();// 기간
+		ie5 = element.iterator();// url
+		String temp[] = ie5.next().html().split("</li>");
+		while (ie1.hasNext()) {
+			String temp2[] = temp[i++].split("'");
+			alba_event.add(new Eventtext(ie1.next().text(), ie2.next().text(), ie3.next().text(), "이벤트", temp2[5]));
+		}
+
 		return alba_event;
 	}
 
