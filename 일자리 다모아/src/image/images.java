ab39a652ffbd0be2ec602b68c6d1655a0e8dd675
ktj97 <ktj97@DESKTOP-OLO68R9>
2021-05-28 20:17:30 +0900

    이미지

---
+++ 일자리 다모아/src/image/images.java
@@ -0,0 +1,28 @@
+package image;
+
+import javax.swing.ImageIcon;
+
+import Function.dirver;
+public class images {
+	public static ImageIcon Albamon = new ImageIcon(dirver.class.getResource("../images/알바몬.png"));
+	public static ImageIcon Dialog_Albamon = new ImageIcon(dirver.class.getResource("../images/다이얼로그_알바몬.png"));
+	public static ImageIcon Albaheaven = new ImageIcon(dirver.class.getResource("../images/알바천국.png"));
+	public static ImageIcon Albaheaven2 = new ImageIcon(dirver.class.getResource("../images/알바천국2.jpg"));
+	public static ImageIcon Repasitory = new ImageIcon(dirver.class.getResource("../images/북마크.png"));
+	public static ImageIcon event1 = new ImageIcon(dirver.class.getResource("../images/event1.jpg"));
+	public static ImageIcon event2 = new ImageIcon(dirver.class.getResource("../images/event2.jpg"));
+	public static ImageIcon IPP = new ImageIcon(dirver.class.getResource("../images/IPP.png"));
+	public static ImageIcon ICT = new ImageIcon(dirver.class.getResource("../images/ICT.png"));
+	public static ImageIcon save = new ImageIcon(dirver.class.getResource("../images/save.png"));
+	public static ImageIcon tip = new ImageIcon(dirver.class.getResource("../images/tip.png"));
+	public static ImageIcon tip2 = new ImageIcon(dirver.class.getResource("../images/팁.png"));
+	public static ImageIcon TIP1 = new ImageIcon(dirver.class.getResource("../images/최저임금.png"));
+	public static ImageIcon TIP2 = new ImageIcon(dirver.class.getResource("../images/적용대상.png"));
+	public static ImageIcon TIP3 = new ImageIcon(dirver.class.getResource("../images/유효성.png"));
+	public static ImageIcon TIP4 = new ImageIcon(dirver.class.getResource("../images/주휴 수당.png"));
+	public static ImageIcon TIP5 = new ImageIcon(dirver.class.getResource("../images/야간,추가수당.png"));
+	public static ImageIcon TIP6 = new ImageIcon(dirver.class.getResource("../images/근로계약서.png"));
+	public static ImageIcon TIP7 = new ImageIcon(dirver.class.getResource("../images/4대보험.png"));
+	public static ImageIcon TIP8 = new ImageIcon(dirver.class.getResource("../images/4대보험질문1.png"));
+	public static ImageIcon TIP9 = new ImageIcon(dirver.class.getResource("../images/4대보험질문2.png"));
+}
