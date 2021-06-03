90173721e9631ed8a33f481714ee7b53d3cf0750
ktj97 <ktj97@DESKTOP-OLO68R9>
2021-06-03 23:30:39 +0900

    인턴 페이지

--- 일자리 다모아/src/GUI/Internpage.java
+++ 일자리 다모아/src/GUI/Internpage.java
@@ -1,6 +1,9 @@
 package GUI;
 
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -12,19 +15,26 @@ import java.util.Vector;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
+import javax.swing.table.TableCellRenderer;
 
 import Function.ICT;
 import Function.IPP;
 import Function.Interncrolling;
 import Function.SaveFunction;
+import image.images;
 
 public class Internpage extends JFrame {
 	static Interncrolling Intern_crolling = new Interncrolling();
+	private static JTableHeader header;
 	static JTable table; // 테이블
 	static DefaultTableModel TableModel = new DefaultTableModel() {// 테이블 내용 수정 불가
 		public boolean isCellEditable(int rowIndex, int mCollndex) {
@@ -34,16 +44,23 @@ public class Internpage extends JFrame {
 	public static DefaultComboBoxModel ComboModel = new DefaultComboBoxModel();
 	static String ICThead[] = { "회사명", "제목", "관련기술", "지역", "배정 인원", "채용 현황" };
 	static String IPPhead[] = { "회사명", "제목", "지역", "모집 인원", "진행 상태" };
-	static String recent_IPPhead[] = { "회사명", "제목", "지역", "모집 인원", "진행 상태","신청 기간" };
+	static String recent_IPPhead[] = { "회사명", "제목", "지역", "모집 인원", "진행 상태", "신청 기간" };
+
 	public static JScrollPane scroll;
 	static Vector<ICT> ICT;
 	static Vector<IPP> IPP;
 	static Object arr[][];
-	JButton SAVE = new JButton("저장");
-	MouseListener MouseListener = new MouseListener();
-	public static JComboBox select;
-	Buttonlistener listener = new Buttonlistener();
-
+	JButton SAVE;
+	JButton BACK;
+	static JLabel title = new JLabel();
+	public JComboBox select;
+	JPanel background = new JPanel() {
+		public void paintComponent(Graphics g) {
+			g.drawImage(images.ground3.getImage(), 0, 0, null); // 사진은 나중에 찾기
+			setOpaque(false);
+			super.paintComponent(g);
+		}
+	};
 	public Internpage() {
 		setTitle("일자리 다모아 - 인턴팝업");
 		setSize(1500, 800);
@@ -51,8 +68,25 @@ public class Internpage extends JFrame {
 		setVisible(true);
 		setResizable(false); // 프로그램 크기 조절 불가 설정
 		setLocationRelativeTo(null); // 화면 중앙에 오도록 하는 설정
-		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		table = new JTable(TableModel);
+
+		background.setLayout(null);
+		setContentPane(background);
+		setBackground(Color.WHITE);
+		
+		table = new JTable(TableModel) {
+			public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
+				JComponent component = (JComponent) super.prepareRenderer(renderer, row, column);
+				if (!isRowSelected(row)) {
+					if (row % 2 == 0)
+						component.setBackground(new Color(213, 227, 207));
+					else
+						component.setBackground(Color.WHITE);
+				}
+				return component;
+
+			}
+		};
+		MouseListener MouseListener = new MouseListener();
 		table.addMouseListener(MouseListener);
 		INTERN_Search(0);
 		scroll = new JScrollPane(table);
@@ -60,19 +94,18 @@ public class Internpage extends JFrame {
 		scroll.setVisible(true);
 		add(scroll);
 
+		title.setIcon(images.title_ict);
+		title.setBounds(380, 0, 500, 130);
+		add(title);
+
 		JLabel label = new JLabel("종류");
 		label.setBounds(40, 90, 50, 30);
 		label.setFont(new Font("맑은 고딕", Font.BOLD, 15));
 		add(label);
 
-		SAVE.setBounds(600, 20, 60, 60);
-		SAVE.setVisible(true);
-		add(SAVE);
-		SAVE.addActionListener(listener);
-
 		String[] kind = { "[ICT] 2021년 상반기(국내과정)", "[ICT] 2020년 하반기(국내과정)", "[ICT] 2020년 상반기(국내과정)",
-				"=============================", "[IPP] 2021년 하반기(국내과정)", "[IPP] 2021년 상반기(국내과정)", "[IPP] 2020년 하반기(국내과정)",
-				"[IPP] 2020년 상반기(국내과정)", "[IPP] 2019년 하반기(국내과정)", "[IPP] 2019년 상반기(국내과정)" };
+				"=============================", "[IPP] 2021년 하반기(국내과정)", "[IPP] 2021년 상반기(국내과정)",
+				"[IPP] 2020년 하반기(국내과정)", "[IPP] 2020년 상반기(국내과정)", "[IPP] 2019년 하반기(국내과정)", "[IPP] 2019년 상반기(국내과정)" };
 		select = new JComboBox(kind);
 		select.setSelectedIndex(0);
 		select.setBounds(100, 90, 250, 30);
@@ -80,37 +113,72 @@ public class Internpage extends JFrame {
 		add(select);
 		combolistener Combolistener = new combolistener();
 		select.addActionListener(Combolistener);
+
+		Buttonlistener listener = new Buttonlistener();
+		SAVE = new JButton();
+		SAVE.setBounds(1200, 30, 100, 100);
+		SAVE.setIcon(images.save);
+		add(SAVE);
+		SAVE.addActionListener(listener);
+		SAVE.setContentAreaFilled(false);
+		SAVE.setBorderPainted(false);
+		
+		
+		BACK = new JButton();
+		BACK.setBounds(1390, 20, 110, 110);
+		BACK.setIcon(images.back);
+		add(BACK);
+		BACK.addActionListener(listener);
+		BACK.setContentAreaFilled(false);
+		BACK.setBorderPainted(false);
+		
+
 	}
 
-	class Buttonlistener implements ActionListener { // 버튼 이벤트
+	public class Buttonlistener implements ActionListener { // 버튼 이벤트
 		public void actionPerformed(ActionEvent e) {
+			System.out.println("Test");
 			if (e.getSource() == SAVE) {
-				if (select.getSelectedIndex() >= 0 && select.getSelectedIndex() <= 2) {
-					try {
-						SaveFunction.saveintern(Loginpage.ID_.getText(), ICT.get(table.getSelectedRow()).getName(),
-								ICT.get(table.getSelectedRow()).getProject(), ICT.get(table.getSelectedRow()).getArea(),
-								ICT.get(table.getSelectedRow()).getNum(), "마감",
-								ICT.get(table.getSelectedRow()).getURL());
-						Repositorypage.Search_Intern();
-					} catch (SQLException e1) {
-					} catch (ArrayIndexOutOfBoundsException e2) {
-					} catch (NullPointerException e3) {
+				if (table.getSelectedRow() != -1) {
+					int connect = JOptionPane.showConfirmDialog(null, "저장 하시겠습니까?", "Notice", JOptionPane.YES_NO_OPTION,
+							JOptionPane.QUESTION_MESSAGE, images.Dialog_save);
+					if (connect == JOptionPane.CLOSED_OPTION)
+						;
+					else if (connect == JOptionPane.YES_OPTION) {
+						if (select.getSelectedIndex() >= 0 && select.getSelectedIndex() <= 2) {
+							try {
+								SaveFunction.saveintern(Loginpage.ID_.getText(),
+										ICT.get(table.getSelectedRow()).getName(),
+										ICT.get(table.getSelectedRow()).getProject(),
+										ICT.get(table.getSelectedRow()).getArea(),
+										ICT.get(table.getSelectedRow()).getNum(), "마감",
+										ICT.get(table.getSelectedRow()).getURL());
+								JOptionPane.showMessageDialog(null, "저장되었습니다.");
+								Repositorypage.Search_Intern();
+							} catch (SQLException e1) {
+							} catch (NullPointerException e2) {
+							}
+						} else {
+							try {
+								SaveFunction.saveintern(Loginpage.ID_.getText(),
+										IPP.get(table.getSelectedRow()).getName(),
+										IPP.get(table.getSelectedRow()).getProject(),
+										IPP.get(table.getSelectedRow()).getArea(),
+										IPP.get(table.getSelectedRow()).getNum(),
+										IPP.get(table.getSelectedRow()).getState(),
+										IPP.get(table.getSelectedRow()).getURL());
+								JOptionPane.showMessageDialog(null, "저장되었습니다.");
+								Repositorypage.Search_Intern();
+							} catch (SQLException e1) {
+							} catch (NullPointerException e2) {
+							}
+						}
 					}
-
-				} else {
-					try {
-						SaveFunction.saveintern(Loginpage.ID_.getText(), IPP.get(table.getSelectedRow()).getName(),
-								IPP.get(table.getSelectedRow()).getProject(), IPP.get(table.getSelectedRow()).getArea(),
-								IPP.get(table.getSelectedRow()).getNum(), IPP.get(table.getSelectedRow()).getState(),
-								IPP.get(table.getSelectedRow()).getURL());
-						Repositorypage.Search_Intern();
-					} catch (SQLException e1) {
-					} catch (ArrayIndexOutOfBoundsException e2) {
-					} catch (NullPointerException e3) {
-					}
-
-				}
+				} else
+					JOptionPane.showMessageDialog(null, "광고를 선택하지 않으셨습니다.");
 			}
+			if (e.getSource() == BACK)
+				dispose();
 		}
 	}
 
@@ -134,9 +202,11 @@ public class Internpage extends JFrame {
 				}
 			}
 		}
+
 	}
 
-	public static void INTERN_Search(int index) {
+	public void INTERN_Search(int index) {
+
 		if (index == 0) {
 			try {
 				ICT = Intern_crolling.ICT2021상반기();
@@ -215,10 +285,12 @@ public class Internpage extends JFrame {
 		public void actionPerformed(ActionEvent e) {
 			INTERN_Search(select.getSelectedIndex());
 		}
-
 	}
 
 	public static void Search_ict() {
+		header = table.getTableHeader();
+		header.setBackground(new Color(99, 162, 239));
+		header.setFont(new Font("맑은 고딕", Font.BOLD, 25));
 		for (int i = 0; i < ICT.size(); i++) {
 			int j = 0;
 			arr[i][j++] = ICT.get(i).getName();
@@ -229,18 +301,22 @@ public class Internpage extends JFrame {
 			arr[i][j++] = ICT.get(i).getNum();
 		}
 		TableModel.setDataVector(arr, ICThead);
-		table.getColumnModel().getColumn(0).setPreferredWidth(250);
+		table.getColumnModel().getColumn(0).setPreferredWidth(230);
 		table.getColumnModel().getColumn(1).setPreferredWidth(525);
 		table.getColumnModel().getColumn(2).setPreferredWidth(250);
 		table.getColumnModel().getColumn(3).setPreferredWidth(170);
 		table.getColumnModel().getColumn(4).setPreferredWidth(120);
-		table.getColumnModel().getColumn(5).setPreferredWidth(80);
+		table.getColumnModel().getColumn(5).setPreferredWidth(100);
 		table.getTableHeader().setReorderingAllowed(false);
 		table.setRowHeight(35);
 		table.setFont(new Font("맑은 고딕", Font.BOLD, 25)); // 글자 크기 설정
+		title.setIcon(images.title_ict);
 	}
 
 	public static void Search_ipp() {
+		header = table.getTableHeader();
+		header.setBackground(new Color(99, 162, 239));
+		header.setFont(new Font("맑은 고딕", Font.BOLD, 25));
 		for (int i = 0; i < IPP.size(); i++) {
 			int j = 0;
 			arr[i][j++] = IPP.get(i).getName();
@@ -250,16 +326,21 @@ public class Internpage extends JFrame {
 			arr[i][j++] = IPP.get(i).getState();
 		}
 		TableModel.setDataVector(arr, IPPhead);
-		table.getColumnModel().getColumn(0).setPreferredWidth(230);
+		table.getColumnModel().getColumn(0).setPreferredWidth(210);
 		table.getColumnModel().getColumn(1).setPreferredWidth(600);
 		table.getColumnModel().getColumn(2).setPreferredWidth(180);
-		table.getColumnModel().getColumn(3).setPreferredWidth(50);
+		table.getColumnModel().getColumn(3).setPreferredWidth(70);
 		table.getColumnModel().getColumn(4).setPreferredWidth(120);
 		table.getTableHeader().setReorderingAllowed(false);
 		table.setRowHeight(35);
 		table.setFont(new Font("맑은 고딕", Font.BOLD, 25)); // 글자 크기 설정
+		title.setIcon(images.title_ipp);
 	}
+
 	public static void Search_ipp_recent() {
+		header = table.getTableHeader();
+		header.setBackground(new Color(99, 162, 239));
+		header.setFont(new Font("맑은 고딕", Font.BOLD, 25));
 		for (int i = 0; i < IPP.size(); i++) {
 			int j = 0;
 			arr[i][j++] = IPP.get(i).getName();
@@ -271,13 +352,14 @@ public class Internpage extends JFrame {
 		}
 		TableModel.setDataVector(arr, recent_IPPhead);
 		table.getColumnModel().getColumn(0).setPreferredWidth(230);
-		table.getColumnModel().getColumn(1).setPreferredWidth(550);
-		table.getColumnModel().getColumn(2).setPreferredWidth(200);
-		table.getColumnModel().getColumn(3).setPreferredWidth(40);
+		table.getColumnModel().getColumn(1).setPreferredWidth(500);
+		table.getColumnModel().getColumn(2).setPreferredWidth(230);
+		table.getColumnModel().getColumn(3).setPreferredWidth(80);
 		table.getColumnModel().getColumn(4).setPreferredWidth(70);
 		table.getColumnModel().getColumn(5).setPreferredWidth(120);
 		table.getTableHeader().setReorderingAllowed(false);
 		table.setRowHeight(35);
 		table.setFont(new Font("맑은 고딕", Font.BOLD, 25)); // 글자 크기 설정
+		title.setIcon(images.title_ipp);
 	}
 }
