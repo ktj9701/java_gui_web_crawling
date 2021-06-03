c57c62265ebf88d62f9b6099e02486208af0252e
ktj97 <ktj97@DESKTOP-OLO68R9>
2021-06-03 23:30:35 +0900

    이미지 파일 모음

--- 일자리 다모아/src/image/images.java
+++ 일자리 다모아/src/image/images.java
@@ -4,16 +4,28 @@ import javax.swing.ImageIcon;
 
 import Function.dirver;
 public class images {
+	public static ImageIcon start = new ImageIcon(dirver.class.getResource("../images/start.png"));
+	public static ImageIcon join = new ImageIcon(dirver.class.getResource("../images/join.png"));
 	public static ImageIcon Albamon = new ImageIcon(dirver.class.getResource("../images/알바몬.png"));
+	public static ImageIcon ground = new ImageIcon(dirver.class.getResource("../images/ground.png"));
+	public static ImageIcon ground2 = new ImageIcon(dirver.class.getResource("../images/ground2.png"));
+	public static ImageIcon ground3 = new ImageIcon(dirver.class.getResource("../images/ground3.jpg"));
+	public static ImageIcon ground4 = new ImageIcon(dirver.class.getResource("../images/ground4.jpg"));
+	public static ImageIcon repository = new ImageIcon(dirver.class.getResource("../images/repository.png"));
 	public static ImageIcon Dialog_Albamon = new ImageIcon(dirver.class.getResource("../images/다이얼로그_알바몬.png"));
 	public static ImageIcon Albaheaven = new ImageIcon(dirver.class.getResource("../images/알바천국.png"));
 	public static ImageIcon Albaheaven2 = new ImageIcon(dirver.class.getResource("../images/알바천국2.jpg"));
+	public static ImageIcon title_Albaheaven = new ImageIcon(dirver.class.getResource("../images/알바천국타이틀.png"));
+	public static ImageIcon title_event = new ImageIcon(dirver.class.getResource("../images/이벤트타이틀.png"));
 	public static ImageIcon Repasitory = new ImageIcon(dirver.class.getResource("../images/북마크.png"));
 	public static ImageIcon event1 = new ImageIcon(dirver.class.getResource("../images/event1.jpg"));
 	public static ImageIcon event2 = new ImageIcon(dirver.class.getResource("../images/event2.jpg"));
 	public static ImageIcon IPP = new ImageIcon(dirver.class.getResource("../images/IPP.png"));
 	public static ImageIcon ICT = new ImageIcon(dirver.class.getResource("../images/ICT.png"));
 	public static ImageIcon save = new ImageIcon(dirver.class.getResource("../images/save.png"));
+	public static ImageIcon Dialog_save = new ImageIcon(dirver.class.getResource("../images/다이얼로그_저장.png"));
+	public static ImageIcon title_ict = new ImageIcon(dirver.class.getResource("../images/ict2.png"));
+	public static ImageIcon title_ipp = new ImageIcon(dirver.class.getResource("../images/ipp1.png"));
 	public static ImageIcon tip = new ImageIcon(dirver.class.getResource("../images/tip.png"));
 	public static ImageIcon tip2 = new ImageIcon(dirver.class.getResource("../images/팁.png"));
 	public static ImageIcon TIP1 = new ImageIcon(dirver.class.getResource("../images/최저임금.png"));
@@ -25,4 +37,8 @@ public class images {
 	public static ImageIcon TIP7 = new ImageIcon(dirver.class.getResource("../images/4대보험.png"));
 	public static ImageIcon TIP8 = new ImageIcon(dirver.class.getResource("../images/4대보험질문1.png"));
 	public static ImageIcon TIP9 = new ImageIcon(dirver.class.getResource("../images/4대보험질문2.png"));
+	public static ImageIcon back = new ImageIcon(dirver.class.getResource("../images/back.png"));
+	public static ImageIcon right = new ImageIcon(dirver.class.getResource("../images/right.png"));
+	public static ImageIcon left = new ImageIcon(dirver.class.getResource("../images/left.png"));
+	public static ImageIcon delete = new ImageIcon(dirver.class.getResource("../images/delete.png"));
 }
