6bbaa1f31c04437461747e42871031285d2fd649
ktj97 <ktj97@DESKTOP-OLO68R9>
2021-06-03 23:30:06 +0900

    시작 클래스

--- 일자리 다모아/src/Function/dirver.java
+++ 일자리 다모아/src/Function/dirver.java
@@ -9,7 +9,6 @@ public class dirver {
 	public static final int SCREEN_WIDTH=1024;  //가로길이
 	public static final int SCREEN_HEIGHT=720; // 세로길이
 	public static void main(String[] args) {
-
 		new Loginpage();
 	} 
 }
